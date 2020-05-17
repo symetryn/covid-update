7e510a49bc1139b089bcacd3d01fea11c4297857
rojan <roseonmhr@gmail.com>
2020-05-17 16:02:43 +0545

    changed status bar color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,9 @@ import { Component } from "@angular/core";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"]
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
   affected = 10000;
-  title = "corona-update";
+  title = "Corona Tracker";
 }
